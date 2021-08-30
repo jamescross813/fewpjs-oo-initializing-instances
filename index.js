7578f8a36517aa3189d17d39bca776328c7203ba
jamescross <crossjames813@gmail.com>
2021-08-30 13:56:40 -0500

    commit tree afe6849d3acc9b01f7e55323d3acd1b3e6fcda9a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+class Breakfast{
+    constructor(food, drink){
+        this.food = food,
+        this.drink = drink
+    }
+}
+
+let one = new Breakfast('eggs')
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad,
+        this.soup = soup,
+        this.drink = drink
+    }
+}
+
+let two = new Lunch("side salad")
+
+class Dinner{
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad,
+        this.soup = soup,
+        this.entree = entree,
+        this._dessert = dessert
+    }
+}
+
+let three = new Dinner("balsamic salad", "", "", "cheesecake")
